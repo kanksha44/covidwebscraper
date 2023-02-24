5a161736c804860e7bc402e08b0d5f0c1230a648
kanksha4496 <kanksha44@gmail.com>
2023-02-24 15:03:39 +0530

    first commit

---
+++ request.js
@@ -0,0 +1,13 @@
+const request = require("request");
+//feature - =>  request
+console.log("before");
+
+request("https://www.worldometers.info/coronavirus/", cb);
+
+console.log("after");
+function cb(error, response, html) {
+  console.error("error:", error); // Print the error if one occurred
+  console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+  console.log("html:", html); // Print the HTML for the Google homepage.
+}
+
