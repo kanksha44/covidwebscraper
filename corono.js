fab1b8e67180a40609d153a7b83306faaf5e78d4
kanksha4496 <kanksha44@gmail.com>
2023-02-25 12:39:36 +0530

    changes

--- corono.js
+++ corono.js
@@ -1,12 +1,13 @@
 const request = require("request");
 const cheerio = require("cheerio");
-const chalk = require("chalk");
+// const {chalk} = require("chalk");
+// import {chalk} from 'chalk';
 //feature - =>  request
-console.log("before");
+// console.log("before");
 
 request("https://www.worldometers.info/coronavirus/", cb);
 
-console.log("after");
+// console.log("after");
 function cb(error, response, html) {
   if (error) {
     console.error("error:", error); // Print the error if one occurred
@@ -16,17 +17,19 @@ function cb(error, response, html) {
 }
 function handlehtml(html) {
   let selTool = cheerio.load(html);
-  let contentArr = selTool(".maincounter-number span");
-  //   console.log(h1s.length);
-    for (let i = 0; i < contentArr.length; i++) {
-      let data = selTool(contentArr[i]).text();
-      console.log("data", data);
-    }
+  // console.log(selTool,"asedf");
+  let contentArr = selTool("#maincounter-wrap span");
+  // console.log(contentArr,"2nd console");
+  for (let i = 0; i < contentArr.length; i++) {
+    ``;
+    let data = selTool(contentArr[i]).text();
+    // console.log("data", data);
+  }
 
   let total = selTool(contentArr[0]).text();
   let deaths = selTool(contentArr[1]).text();
   let recovered = selTool(contentArr[2]).text();
-  console.log(chalk.gray("Total cases:" + total));
-  console.log(chalk.red("Deaths:" + deaths));
-  console.log(chalk.green("Recovered:" + recovered));
+  console.log("Total cases:" + total);
+  console.log("Deaths:" + deaths);
+  console.log("Recovered:" + recovered);
 }
